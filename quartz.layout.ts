bab9679dc553f01faeab9cba857cdf6dada2c483
gareth <gareth.williams82@yahoo.co.uk>
2024-11-21 21:46:51 +0000

    Quartz sync: Nov 21, 2024, 9:46 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,7 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
-      "Can't see on mobile devices?": "Use_a_PC!",
+      "Can't see on mobile devices?": "Use_a_PC",
     },
   }),
 }
