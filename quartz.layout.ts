11de5361ae9991a145fdd091a52aec9d8126449d
gareth <gareth.williams82@yahoo.co.uk>
2025-01-17 15:37:23 +0000

    Quartz sync: Jan 17, 2025, 3:37 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,7 +1,7 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg";
-import * as Component from "./quartz/components";
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
 
-// Shared components across all pages
+// components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
@@ -9,13 +9,13 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       // GitHub: "https://github.com/jackyzha0/quartz",
-      // "Discord Community": "https://discord.gg/UzQuaZfa",
+      //"Discord Community": "https://discord.gg/UzQuaZfa",
       // "Can't see on mobile devices?": "Use a PC!",
     },
   }),
-};
+}
 
-// Layout for pages displaying a single page (e.g., a single note)
+// components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -27,26 +27,24 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    // Include Darkmode component with initial state 'dark' 
-    Component.Darkmode({ initialState: 'dark' }), 
+    // Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
     // Component.Backlinks(),
   ],
-};
+}
 
-// Layout for pages displaying lists of pages (e.g., tags or folders)
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    // Include Darkmode component with initial state 'dark' 
-    Component.Darkmode({ initialState: 'dark' }), 
+    Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
-};
+}
